1ae0a3955b6a2881bc58371b4c85944d83b82205
Allan Farca <allan.farca@gmail.com>
2020-04-21 09:22:46 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize (sender, receiver, amount = 50)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end 
+  
+  def valid?
+    if (@sender.valid? == true) && 
+        (@receiver.valid? == true)
+        return true
+      else 
+        return false 
+      end 
+  end 
+  
+  def execute_transaction
+  end 
+  
+  def reverse_transfer
+  end 
+  
 end
