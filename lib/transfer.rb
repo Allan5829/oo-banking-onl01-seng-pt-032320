6f469f02911856bade60e6348d722bb0608008c9
Allan Farca <allan.farca@gmail.com>
2020-04-22 03:03:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,8 +33,8 @@ class Transfer
   
   def reverse_transfer
     if (@status == "complete")
-      @sender.balance -= @amount
-      @receiver.balance += @amount
+      @receiver.withdraw (@amount)
+      @sender.deposit (@amount)
       @status = "reversed"
     end 
   end 
