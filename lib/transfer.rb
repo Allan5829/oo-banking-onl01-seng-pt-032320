e476edd09393d95641dbdcdce8d28c14bdb47f45
Allan Farca <allan.farca@gmail.com>
2020-04-22 02:43:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,14 +18,14 @@ class Transfer
   end 
   
   def execute_transaction
-    if valid? == true && @status == "pending"
+    #if valid? == true && @status == "pending"
       @sender.withdraw (@amount)
       @receiver.deposit (@amount)
       @status = "complete"
-    elsif (valid? == false) || @status == "complete" 
-      @status = "rejected"  
-      return "Transaction rejected. Please check your account balance."
-    end 
+    #elsif (valid? == false) || @status == "complete" 
+     # @status = "rejected"  
+     # return "Transaction rejected. Please check your account balance."
+    #end 
   end 
   
   def reverse_transfer
