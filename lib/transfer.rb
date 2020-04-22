8a737f5e00390cb2ce26cfb9043cf0eb8611afd7
Allan Farca <allan.farca@gmail.com>
2020-04-22 03:09:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif (@sender.balance < @amount)
+    elsif (@sender.status = "closed")
       @status = "rejected"  
       return "Transaction rejected. Please check your account balance."
     end 
